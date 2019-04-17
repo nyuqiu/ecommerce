a7887d61f53e3129c3750ffcda83cdd7b5cb8a5e
k.dabrowski8712 <k.dabrowski8712@gmail.com>
2019-04-17 21:27:48 +0200

    JDP190302-39 poprawka Cart

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,22 +1,23 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AccessLevel;
-import lombok.Setter;
+import lombok.*;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Setter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PUBLIC)
 @Entity
 @Table(name="CARTS")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart   {
 
     private Long id;
-    private List<Product> products;
-
-    public Cart() {
-        products = new ArrayList<>();
-    }
+    private List<Product> products = new ArrayList<>();
+    private User user;
+    private Order order;
 
     @OneToMany( targetEntity = Product.class,
                 mappedBy = "cart",
@@ -25,8 +26,21 @@ public class Cart   {
         return products;
     }
 
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_ID")
+    public User getUser() {
+        return user;
+    }
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="ORDER_ID")
+    public Order getOrder() {
+        return order;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
     public Long getId() {
         return id;
     }
