f11da72675646a7f30ec0b315e3e1c797f7f614b
Lipien <adam@pawtel.pl>
2019-04-18 06:44:22 +0200

    JDP190302-24 OrderController - rzeczywista implementacja.

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -4,6 +4,20 @@ import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository <Order, Long> {
+    @Override
+    List <Order> findAll();
+
+    @Override
+    Optional <Order> findById(Long id);
+
+    @Override
+    void deleteById(Long id);
+
+    @Override
+    Order save(Order order);
 }
