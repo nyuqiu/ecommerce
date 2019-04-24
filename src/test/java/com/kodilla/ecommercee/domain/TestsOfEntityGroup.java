85428321db8370b7d688f24530af73dd87b440ef
jarhuba@gmail.com <Dragon1Gandalf1>
2019-04-24 09:09:50 +0200

    Poprawki testów encji Group plus metoda getGroupByName, deleteById

--- src/test/java/com/kodilla/ecommercee/domain/TestsOfEntityGroup.java
+++ src/test/java/com/kodilla/ecommercee/domain/TestsOfEntityGroup.java
@@ -22,59 +22,49 @@ public class TestsOfEntityGroup {
     @Autowired
     private GroupRepository groupRepository;
 
-    private Group group;
-    private Group group2;
+    private Group groupComputers;
+    private Group groupAgd;
 
     @Before
     public void setUp() {
         //Given
         Product product1 = new Product();
-        Long id = product1.getId();
-        product1.setId(id);
         product1.setTitle("Acer 6574");
         product1.setContent("laptop for business");
         product1.setPrice(3000);
 
         Product product2 = new Product();
-        Long id2 = product2.getId();
-        product2.setId(id2);
         product2.setTitle("Dell 7878");
         product2.setContent("PC");
         product2.setPrice(2500);
 
         Product product3 = new Product();
-        Long id3 = product3.getId();
-        product3.setId(id3);
         product3.setTitle("Mastercook");
         product3.setContent("modern cooker");
         product3.setPrice(1000);
 
         Product product4 = new Product();
-        Long id4 = product4.getId();
-        product4.setId(id4);
         product4.setTitle("FridgeBeko");
         product4.setContent("modern fridge class A+");
         product4.setPrice(1500);
 
-        List<Product> productsNew = new ArrayList<>();
-        productsNew.add(product1);
-        productsNew.add(product2);
-        List<Product> productsOld = new ArrayList<>();
-        productsNew.add(product3);
-        productsOld.add(product4);
+        List<Product> productsComputers = new ArrayList<>();
+        productsComputers.add(product1);
+        productsComputers.add(product2);
+        List<Product> productsAgd = new ArrayList<>();
+        productsAgd.add(product3);
+        productsAgd.add(product4);
 
-        group = new Group();
-        group.setId(1L);
-        group.setGroupName("media");
-        group.setProducts(productsNew);
+        groupComputers = new Group();
+        groupComputers.setGroupName("computers");
+        groupComputers.setProducts(productsComputers);
 
-        group2 = new Group();
-        group2.setId(2L);
-        group2.setGroupName("agd");
-        group2.setProducts(productsOld);
+        groupAgd = new Group();
+        groupAgd.setGroupName("agd");
+        groupAgd.setProducts(productsAgd);
 
-        groupRepository.save(group);
-        groupRepository.save(group2);
+        groupRepository.save(groupComputers);
+        groupRepository.save(groupAgd);
     }
 
     @Test
@@ -82,7 +72,7 @@ public class TestsOfEntityGroup {
 
         //When
         List<Group> groups = groupRepository.findAll();
-        double price = group.getProducts().get(1).getPrice();
+        double price = groupComputers.getProducts().get(1).getPrice();
 
         //Then
         Assert.assertEquals(2, groups.size());
@@ -93,7 +83,7 @@ public class TestsOfEntityGroup {
     public void testReadGroupById() {
 
         //When
-        Long id = group.getId();
+        Long id = groupComputers.getId();
         Optional<Group> readGroups = groupRepository.findById(id);
 
         //Then
@@ -105,24 +95,25 @@ public class TestsOfEntityGroup {
     public void testUpdateGroupName() {
 
         //When
-        Long id = group.getId();
-        group.setGroupName("computers");
+        groupComputers.setGroupName("computersRebranded");
+        Optional<Group> groupRebranded = groupRepository.findByGroupName(groupComputers.getGroupName());
+        String nameRebranded = groupRebranded.get().getGroupName();
 
         //Then
-        Assert.assertEquals("computers", group.getGroupName());
+        Assert.assertEquals("computersRebranded", nameRebranded);
     }
 
-//    @Test
-//    public void testDeleteById() {
-//
-//        //When
-//        Long id = group.getId();
-//        Long id2 = group2.getId();
-//        groupRepository.deleteById(id);
-//        groupRepository.deleteById(id2);
-//        List<Group> groups = groupRepository.findAll();
-//
-//        //Then
-//        Assert.assertEquals(0, groups.size());
-//    }
+    @Test
+    public void testDeleteById() {
+
+        //When
+        Long idComputers = groupComputers.getId();
+        Long idAgd = groupAgd.getId();
+        groupRepository.deleteById(idComputers);
+        groupRepository.deleteById(idAgd);
+        List<Group> groups = groupRepository.findAll();
+
+        //Then
+        Assert.assertEquals(0, groups.size());
+    }
 }
